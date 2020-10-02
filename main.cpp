63dcd0cbfc6bae37105405be4e9a537cfab65986
Daniel Neri <daniel.neri5575@alumnos.udg.mx>
2020-10-02 02:28:48 -0500

    ya agregue todas los tipos de datos falatantes short, float, double y long

--- main.cpp
+++ main.cpp
@@ -11,8 +11,24 @@ int main() {
     }
     if (cadena=="char"){
         cout << sizeof(char) << endl;
-
     }
+
+    if (cadena=="short"){
+        cout << sizeof(short) << endl;
+    }
+
+    if (cadena=="float"){
+        cout << sizeof(float) << endl;
+    }
+
+    if (cadena=="double"){
+        cout << sizeof(double) << endl;
+    }
+
+    if (cadena=="long"){
+        cout << sizeof(long) << endl;
+    }
+
 
     return 0;
 }
